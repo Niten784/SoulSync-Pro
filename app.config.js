65a546625a185ba110bbeaacbf02cef1727c5a6e
Niten784 <134975497+Niten784@users.noreply.github.com>
2026-07-02 12:52:56 +0530

    Create app.config.js

---
+++ app.config.js
@@ -0,0 +1,13 @@
+export default {
+  expo: {
+    name: "SoulSync Pro",
+    slug: "soulsync-pro",
+    version: "1.0.0",
+    android: {
+      compileSdkVersion: 35,
+      targetSdkVersion: 35,
+      buildToolsVersion: "35.0.0",
+      package: "com.niten.soulsyncpro"
+    }
+  }
+}
